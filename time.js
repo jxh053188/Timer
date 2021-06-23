0834524d1e7333608703c7366d680a2d553ebc2b
jxh0531 <jarred.harkness@gmail.com>
2021-06-23 10:24:59 +0200

    first commit

---
+++ time.js
@@ -0,0 +1,53 @@
+class Timer {
+    constructor(durationInput, startButton, pauseButton, callbacks) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+        if (callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+
+    start = () => {
+        if (this.onStart) {
+            this.onStart(this.timeRemaining);
+        }
+        this.tick();
+        this.countdown = setInterval(this.tick, 50);
+
+    };
+
+    pause = () => {
+        clearInterval(this.countdown);
+    }
+
+    tick = () => {
+        if (this.timeRemaining <= 0) {
+            this.pause();
+            if (this.onComplete) {
+                this.onComplete();
+            }
+        } else {
+            const timeRemaining = this.timeRemaining;
+            this.timeRemaining = timeRemaining - .05;
+            if (this.onTick) {
+                this.onTick(this.timeRemaining);
+            }
+        }
+
+    }
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);
+    }
+
+    set timeRemaining(time) {
+        this.durationInput.value = time.toFixed(2);
+    }
+
+}
